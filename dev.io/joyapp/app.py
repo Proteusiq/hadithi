6ef9bb4e9ef42a8ce60745592ea800d5ba241a3d
Prayson W. Daniel <praysonpi@gmail.com>
2023-09-09 15:47:48 +0200

    issue with multiple workers

--- dev.io/joyapp/app.py
+++ dev.io/joyapp/app.py
@@ -28,26 +28,37 @@ def startup_event():
     if not model_file.exists():
         from ml import penguins_model
 
-        app.state.ml = penguins_model()
+        ml = penguins_model()
+        ml.meta = {"predictions": 0,
+                   "learned": 0}
+        
+        app.state.ml = ml
     else:
         app.state.ml = pickle.loads(model_file.read_bytes())
 
 
 @app.post("/predict")
-def predict(features: Features) -> str:
+def predict(features: Features) -> dict[str, str | int | None]:
     X = features.model_dump()
-    return app.state.ml.predict_one(X)
+    y_pred  = app.state.ml.predict_one(X)
+    app.state.ml.meta["predictions"] += 1
+
+    return {"predicted": y_pred,
+            **app.state.ml.meta,}
 
 
 @app.post("/learn")
-def learn(learn_features: LearnFeatures) -> dict[str, str]:
+def learn(learn_features: LearnFeatures) -> dict[str, str|int]:
     X = learn_features.features.model_dump()
     y = learn_features.species
 
     y_pred = app.state.ml.predict_one(X)
     app.state.ml.learn_one(X, y)
 
+    app.state.ml.meta["learned"] += 1
+
     with Path(MODEL_FILE).open("wb") as f:
         pickle.dump(app.state.ml, f)
 
-    return {"msg": f"we learned {y}. We initially predicted {y_pred}"}
+    return {"msg": f"we learned {y}. We initially predicted {y_pred}",
+            **app.state.ml.meta,}
